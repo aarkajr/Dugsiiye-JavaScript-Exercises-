7afda8a1bea3922affab3f8bd5b5fb32de4903b7
aarkajr <aarkajr03@gmail.com>
2026-03-17 14:56:48 +0300

    added two files js and html file

---
+++ Exercise-1/main.js
@@ -0,0 +1,17 @@
+
+
+//   ===== First JavaScript Exercise =====//
+
+let x = 7;
+let y = 3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x++);
+console.log(x--);
+
+console.log(y++);
+console.log(y--);
