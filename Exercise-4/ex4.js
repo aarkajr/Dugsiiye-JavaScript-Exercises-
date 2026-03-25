64b95341ca505d34980b0f2312bb9e4f4166ffb4
aarkajr <aarkajr03@gmail.com>
2026-03-25 23:13:47 +0300

    added js Exercise 4 containing two parts

---
+++ Exercise-4/ex4.js
@@ -0,0 +1,30 @@
+//  EXERCISE#4  
+
+// N.B   I used function return 
+
+
+function add(a,b){
+    return(a+b)
+}
+console.log(add(10,10))   // result 20
+console.log(add(20,20))   // result 40
+console.log(add(40,30))   // result 70
+
+let output = add(10,10)
+console.log(output / 2)   // result 10
+console.log(output * 2)   // result 40
+console.log(output - 2)    // result 18
+
+
+
+// EXERCISE-4 part 2
+
+// Here is  function Expression 
+
+const ToAdd = function(a,b){
+    console.log(a+b)
+    
+}
+ToAdd(100,10)  // result 110
+ToAdd(1,10)    // result 11
+ToAdd(12,10)   // result 22
