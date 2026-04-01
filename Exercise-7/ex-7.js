04daa65b506357560eea3fa89e9e4cdae15c00c3
aarkajr <aarkajr03@gmail.com>
2026-04-01 20:47:56 +0300

    added js Exercise 7

---
+++ Exercise-7/ex-7.js
@@ -0,0 +1,15 @@
+
+// EXERCISE   #7
+console.log("Before Push")
+let colors = ["White", "Black", "Yellow"];
+console.log(colors)
+
+
+colors.push("Green")
+console.log("Afer Push, Green color added at the end of the list ")
+console.log(colors)
+
+
+console.log("After shift, the first white color is gone.")
+colors.shift()
+console.log(colors)
