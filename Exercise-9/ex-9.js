c5a34d71b6011381ae4c8fad8e72f1246fc3bcec
aarkajr <aarkajr03@gmail.com>
2026-04-01 20:55:49 +0300

    added js Exercise 9

---
+++ Exercise-9/ex-9.js
@@ -0,0 +1,21 @@
+// EXERCISE   #9
+
+
+let car = {
+    CompanyMade : "Ferrari",
+    Model       : "488 GTB",
+    Year        : 2019,
+    start         : function(){
+        console.log("The car has started" )
+    }
+   
+}
+
+
+
+console.log(car.CompanyMade)
+console.log(car.Model)
+console.log(car.Year)
+console.log(car.start())
+
+
