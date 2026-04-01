025ccbcc661d76083f00fdf8259dd2b34178a56f
aarkajr <aarkajr03@gmail.com>
2026-04-01 20:51:05 +0300

    added js Exercise 7

---
+++ Exercise-8/ex-8.js
@@ -0,0 +1,17 @@
+
+// EXERCISE   #8
+
+
+
+let car = {
+    CompanyMade : "Ferrari",
+    Model       : "488 GTB",
+    Year        : 2019
+}
+console.log(car.CompanyMade)
+console.log(car.Model)
+console.log(car.Year)
+
+console.log(car["CompanyMade"])
+console.log(car["Model"])
+console.log(car["Year"])
