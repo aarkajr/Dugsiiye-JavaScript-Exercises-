b6ac2025f1aa186c5ed0042cc4a3d0bbc309721c
aarkajr <aarkajr03@gmail.com>
2026-04-01 21:24:38 +0300

    added js Exercise 10

---
+++ Exercise-10/ex-10.js
@@ -0,0 +1,21 @@
+
+//   EXERCISE 10
+
+
+let library = [
+
+    {
+        title  :  "Atomic Habits",
+        year   :   2018,
+        Author :   "James Clear"
+    },
+    {
+        title  :  "Deep Work",
+        year   :   2016,
+        Author :   "Cal Newport"
+    }
+]
+
+
+console.log(library[0].title)
+console.log(library[1].Author)
