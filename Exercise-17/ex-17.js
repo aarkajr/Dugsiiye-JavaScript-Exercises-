c4d001e6f9633dbb04fa67a4488b626f611a5ce3
aarkajr <aarkajr03@gmail.com>
2026-04-16 17:11:40 +0300

    added js Exercise 17

---
+++ Exercise-17/ex-17.js
@@ -0,0 +1,15 @@
+// EXERCISE 17
+
+let Temperature  = 29;
+
+if(Temperature < 0){
+    console.log("Verry Cold");
+
+} else if( Temperature < 15 ){
+       console.log("Cold");
+
+}else if( Temperature < 25 ){
+       console.log("Warm");
+}
+else{
+        console.log("Hot");}
