60a8f10ebe1c38871ae64541c8fc231d343d422b
aarkajr <aarkajr03@gmail.com>
2026-04-22 12:25:43 +0300

    added js Exercise 22

---
+++ Exercise-22/ex-22.js
@@ -0,0 +1,14 @@
+// Exercise 22
+
+const colors = ["Red", "green", "Blue"]
+
+const [color1, color2, color3] = colors;
+
+console.log(color1)
+console.log(color3)
+
+//  OR
+
+console.log("------------------- Using one line")
+
+console.log(color1, color3)
