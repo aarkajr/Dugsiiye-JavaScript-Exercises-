6fa6faf9fe11f1d213f2ce7c618f1cb6f12d6ab7
aarkajr <aarkajr03@gmail.com>
2026-05-15 16:00:48 +0300

    added js Exercise 33

---
+++ Exercise-33/ex-33.js
@@ -0,0 +1,12 @@
+//   Exercise 33
+const article = document.querySelector(".article");
+    
+ function knowME() {
+    
+
+    article.innerHTML = `
+      Ahmed is a Frontend Developer with over 2 years of hands-on experience building
+      modern, responsive websites and web applications. I specialize in turning
+      design concepts into clean, pixel-perfect code.
+    `;
+  }
